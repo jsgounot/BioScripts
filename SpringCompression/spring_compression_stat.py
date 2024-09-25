54b0101fdd53e84c7ca8956f865569ca57df78bc
Jean-Sebastien Gounot <jsgounot@gmail.com>
2024-09-25 20:29:02 +0800

    Fix old spring compressing stat bug

--- SpringCompression/spring_compression_stat.py
+++ SpringCompression/spring_compression_stat.py
@@ -2,7 +2,7 @@
 # @Author: jsgounot
 # @Date:   2021-07-09 12:39:20
 # @Last Modified by:   jsgounot
-# @Last Modified time: 2021-07-09 12:49:59
+# @Last Modified time: 2024-09-25 20:27:44
 
 import os
 import pandas as pd
@@ -11,11 +11,26 @@ fnames = snakemake.input
 result = []
 
 for fname in fnames :
-    name = os.path.splitext(os.path.basename(fname))[0]
-    initial_file = os.path.join("./input", name + ".fastq.gz")
+    
+    name = os.path.basename(fname)[:-11]
 
-    spring_size = os.path.getsize(fname)
-    intial_size = os.path.getsize(initial_file)
+    with open(fname) as f:
+        lines = [line.strip().split() for line in f]
+        assert len(lines) == 3 
+
+        spring_size = intial_size = 0
+        spring = False
+
+        for line in lines:
+            if line[1].endswith('.spring'):
+                assert spring == False
+                spring = True
+                spring_size = int(line[0])
+            else:
+                intial_size += int(line[0])
+
+        assert spring_size != 0
+        assert intial_size != 0
 
     result.append({
         "name" : name,
@@ -27,7 +42,7 @@ df = pd.DataFrame(result)
 
 def sizeof_fmt(num, suffix='B'):
     # https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
-    for unit in ['','Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
+    for unit in ['Ki','Mi','Gi','Ti','Pi','Ei','Zi']:
         if abs(num) < 1024.0:
             return "%3.1f%s%s" % (num, unit, suffix)
         num /= 1024.0
